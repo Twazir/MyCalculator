c39028f4cc6559004909cdba9b1bb17d06867138
TMW <77063082+Twazir@users.noreply.github.com>
2024-06-28 18:11:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,21 +4,18 @@
 //:::::::::::: THIS WAS THE "+ - * %" PART , Now let focus on the Numbers :::::::::/
 
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::/
-
+// We chose the "const" for fixed values
 const display = document.getElementById("display");
-
+//Values to input
 function appendToDisplay(input){
     display.value += input;
-
 }
 
-
+// Its time to clear our screen
 function clearDisplay (){
     display.value = ""
-
 }
-
-
+// Thanks to @BroCode Videos, it helped me alot.
 function calculate(){
     try {
          display.value = eval(display.value);
