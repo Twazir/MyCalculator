fd147311ed156984518853f9ed69010125322c20
TMW <77063082+Twazir@users.noreply.github.com>
2024-06-28 17:59:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::/
+//:::::::::::: THIS WAS THE "+ - * %" PART , Now let focus on the Numbers :::::::::/
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::/
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+
+}
+
+
+function clearDisplay (){
+    display.value = ""
+
+}
+
+
+function calculate(){
+    try {
+         display.value = eval(display.value);
+    }
+     catch(error){
+         display.value = "Error"
+     }
+}
+    
